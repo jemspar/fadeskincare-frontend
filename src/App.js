6dc56dc6ab536da469d2df624f06ac0b3fb43eac
Jeremy Sparagon <j.a.sparagon@outlook.com>
2020-10-26 16:29:30 -0700

    blur background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+    
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://fadeskincare.co/images/fade_logo_web.svg" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +18,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <main id="content" />
     </div>
   );
 }
