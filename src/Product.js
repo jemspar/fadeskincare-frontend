e372ac54815a988a171dbb687431659500136fd6
Jeremy Sparagon <j.a.sparagon@outlook.com>
2020-11-01 15:28:20 -0800

    ingredients pt 2

--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,41 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import classnames from 'classnames';
 
 
 class Product extends React.Component {
 
+  state = {
+    showBG2: false,
+  }
+
+  toggleBG = () => {
+    this.setState( prevState => (
+      {
+        showBG2: !prevState.showBG2,
+      }
+    ));
+  };
+
 render() {
+
   const BGImage1 = require('./images/'+this.props.prod.primaryImage);
   const BGImage2 = require('./images/'+this.props.prod.secondaryImage);
-  console.log(BGImage2);
+
   const bgVar = {
     '--background-src-1': 'url('+BGImage1.default+')',
     '--background-src-2': 'url('+BGImage2.default+')',
-  }
+  };
 
     return(
       <div
-      className="product" style={
+      className={
+        classnames('product', {'showBG2': this.state.showBG2})
+      }
+      style={
         bgVar
       }
+      onClick={this.toggleBG}
       >
         <p>{this.props.prod.name}</p>
         <p className="price">${this.props.prod.price}</p>
