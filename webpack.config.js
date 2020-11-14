2e8643ab82a2fd71d70ccaefb7175084fceb284c
Jeremy Sparagon <j.a.sparagon@outlook.com>
2020-11-13 23:04:36 -0800

    webpack working ALMOST!

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,76 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   context: path.join(__dirname, 'src'),
   entry: [
     './index.js',
   ],
   output: {
-    path: path.join(__dirname, 'www'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
+    publicPath: '/public'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.m?js$/,
         exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }],
+              '@babel/preset-react'
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties'
+            ]
+          }
+        }
       },
-    ],
-  },
+      {
+        test: [/\.png$/, /\.jpg$/, /\.jpe?g$/],
+        use: {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            }
+          }
+      },
+      {
+        test: /\.yaml$/,
+        use: {
+          loader: 'yaml-loader'
+        }
+      }
+    ]
+  }
 };
+
+// prev use:
+
+// use: {
+//         loader: 'babel-loader',
+//         options: {
+//           presets: [
+//             [
+//               "@babel/preset-env",
+//               {
+//                 targets: {
+//                   node: "10"
+//                 }
+//               }
+//             ],
+//             "@babel/preset-react"
+//           ]
+//         }
+//       }
+
+
+// babel.config.js
+
+// "@babel/react",
+// {
+//   "modules": false
+// }
